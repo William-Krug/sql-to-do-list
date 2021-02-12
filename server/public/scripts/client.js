a9e91ee932891e1c2c7d91baaa277f366a41cb85
William Krug <william.p.krug@gmail.com>
2021-02-12 08:12:48 -0600

    "In Progress" button listener and handler set up

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,6 +2,15 @@ console.log('*** I have JavaScript 🦖 ***');
 
 $(document).ready(onReady);
 
+const verbose = true;
+
 function onReady() {
   console.log('*** I have jQuery 🦍 ***');
+  $('.status').on('click', '.inProgressButton', moveToInProgress);
+}
+
+function moveToInProgress() {
+  if (verbose) {
+    console.log('*** in moveToInProgress() ***');
+  }
 }
