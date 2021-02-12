9683987fedcf8cf1a6062bfdb74da444d5564347
William Krug <william.p.krug@gmail.com>
2021-02-12 08:15:09 -0600

    "completd" button event listener and handler set up

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -6,11 +6,20 @@ const verbose = true;
 
 function onReady() {
   console.log('*** I have jQuery 🦍 ***');
+
+  // Event Listeners
   $('.status').on('click', '.inProgressButton', moveToInProgress);
+  $('.status').on('click', '.completeButton', moveToCompleted);
 }
 
 function moveToInProgress() {
   if (verbose) {
     console.log('*** in moveToInProgress() ***');
+  }
+}
+
+function moveToCompleted() {
+  if (verbose) {
+    console.log('*** in moveToCompleted() ***');
   }
 }
