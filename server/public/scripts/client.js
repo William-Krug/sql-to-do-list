e1b4b40497f61b8dbc70c214b4bd973b79fe61e3
William Krug <william.p.krug@gmail.com>
2021-02-12 14:58:33 -0600

    POST call created

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,11 +1,11 @@
-console.log('*** I have JavaScript 🦖 ***');
+console.log('*** I have JavaScript ***');
 
 $(document).ready(onReady);
 
 const verbose = true;
 
 function onReady() {
-  console.log('*** I have jQuery 🦍 ***');
+  console.log('*** I have jQuery ***');
 
   // New Task Event Listener
   $('#addTaskForm').on('submit', createTask);
@@ -16,26 +16,75 @@ function onReady() {
   $('.status').on('click', '.deleteButton', removeTask);
 }
 
+/**
+ * Function grabs the user inputs from the DOM and stores the 
+ * inputs inside a new object
+ * 
+ * @param {*} event 
+ */
 function createTask(event) {
+  // Keep the DOM from refreshing
   event.preventDefault();
+
+  const newTask = {
+    taskName: $('#taskNameInput').val(),
+    taskNote: $('#taskNotesInput').val(),
+  };
+
+  // Testing and debugging breadcrumbs
   if (verbose) {
     console.log('*** in createTask() ***');
+    console.log('\tnewTask:', newTask);
   }
+
+  clearInputs();
+}
+
+function postTask(task) {
+  // Testing and debugging breadcrumbs
+  if (verbose) {
+    console.log('*** in putTask() ***');
+    console.log('\ttask:', task);
+  }
+
+  $.ajax({
+    type: 'POST',
+    url: '/todoList',
+    data: task,
+  })
+  .then(function (response) {
+    console.log('POST response:', response);
+    //render function
+  }).catch(function (error) {
+    console.log('*** ERROR in task POST', error);
+    alert('*** ERROR adding task.  Please try again later. ***');
+  });
+}
+
+/**
+ * Function clears out the task creation inputs
+ */
+function clearInputs() {
+  $('#taskNameInput').val(''),
+  $('#taskNotesInput').val(''),
 }
 
 function moveToInProgress() {
+  // Testing and debugging breadcrumbs
   if (verbose) {
     console.log('*** in moveToInProgress() ***');
   }
 }
 
 function moveToCompleted() {
+  // Testing and debugging breadcrumbs
   if (verbose) {
     console.log('*** in moveToCompleted() ***');
   }
 }
 
 function removeTask() {
+  // Testing and debugging breadcrumbs
   if (verbose) {
     console.log('*** in removeTask() ***');
   }
